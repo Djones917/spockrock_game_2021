03d1e1b5920968b7f7a0cc53b3ec5c5515481418
Derek <nightwing917@hotmail.com>
2021-06-18 19:09:00 -0600

    added computer dom elements

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const playerScoreEl = document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
